13fc7103cbbe8dd4afbca6338b202dacae74518d
Shuaib464 <shuaibshekh464@gmail.com>
2026-01-17 19:06:39 +0530

    Hover on a web Element

---
+++ automation-tut/src/test/java/actions_class/Fb_Hover.java
@@ -0,0 +1,28 @@
+package actions_class;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Fb_Hover {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://www.facebook.com/");
+		
+		Thread.sleep(2500);
+		
+		WebElement createBtn = driver.findElement(By.xpath("//a[text()='Forgotten password?']"));
+		
+		Thread.sleep(2500);
+		driver.quit();
+	}
+
+}
